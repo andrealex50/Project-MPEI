fc2244d9069f0970dd8ea8c68648425924fb5f2f
tito-04 <tiagojcm04@ua.pt>
2024-12-14 22:15:50 +0000

    Changes agressividade

--- agressividade.m
+++ agressividade.m
@@ -2,11 +2,11 @@ function agressividade()
 
     mensagens_a_analisar = 'mensagens.txt';  % Mensagens no geral, ficheiro principal
     % (1) Bloom Filter
-    vetor_binario = bloom_filter(mensagens_a_analisar); % Chama a função bloom_filter que retorna o vetor binário
+    mensagens_possiveis = bloom_filter(mensagens_a_analisar); % Chama a função bloom_filter que retorna o vetor binário
 
     % (2) minHash
     mensagens_treino = 'aggressive_texts.json';    % Mensagens que só contém asneiras
-    mensagens_sem_similaridade = minHash(vetor_binario, mensagens_a_analisar, mensagens_treino);   % Retorna mensagens que não encontraram similaridade
+    mensagens_sem_similaridade = minHash(mensagens_possiveis, mensagens_treino);   % Retorna mensagens que não encontraram similaridade
 
     % (3) Naive Bayes
     if ~isempty(mensagens_sem_similaridade)
