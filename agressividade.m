15aa7c5a92515cbc9105237d0fabd78e4f5807a2
andrealex50 <alexandreandre@ua.pt>
2024-12-15 12:57:40 +0000

    acertar filtro de bloom

--- agressividade.m
+++ agressividade.m
@@ -1,13 +1,16 @@
 function agressividade()
 
     mensagens_a_analisar = 'mensagens.txt';  % Mensagens no geral, ficheiro principal
+    disp("---Bloom Filter---");
     % (1) Bloom Filter
     mensagens_possiveis = bloom_filter(mensagens_a_analisar); % Chama a função bloom_filter que retorna o vetor binário
 
+    disp("---MinHash---");
     % (2) minHash
     mensagens_treino = 'aggressive_texts.json';    % Mensagens que só contém asneiras
     mensagens_sem_similaridade = minHash(mensagens_possiveis, mensagens_treino);   % Retorna mensagens que não encontraram similaridade
 
+    disp("---Naive Bayes---");
     % (3) Naive Bayes
     if ~isempty(mensagens_sem_similaridade)
         % Chama a função naive_bayes com as mensagens sem similaridade 
