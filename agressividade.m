6d72c87b9b74e965ed828af9451331b276216b04
tito-04 <tiagojcm04@ua.pt>
2024-12-15 22:15:24 +0000

    Pumba

--- agressividade.m
+++ agressividade.m
@@ -8,7 +8,7 @@ function agressividade()
     disp("---MinHash---");
     % (2) minHash
     mensagens_treino = 'DataSets/aggressive_texts.json';    % Mensagens que só contém asneiras
-    mensagens_sem_similaridade = minHash(mensagens_possiveis, mensagens_treino);   % Retorna mensagens que não encontraram similaridade
+    mensagens_sem_similaridade = minhash_similarity(mensagens_possiveis, mensagens_treino);   % Retorna mensagens que não encontraram similaridade
 
     disp("---Naive Bayes---");
     % (3) Naive Bayes
--- minHash.m
+++ minHash.m
@@ -6,7 +6,6 @@ function mensagens_sem_similaridade = minHash(mensagens, caminho_json_treino)
     mensagens_treino = {mensagens_treino_struct.content}'; % Extrair os textos das mensagens
 
     limiar_similaridade = 0.7;
-
     k = 100; % Número de funções de dispersão
 
     % Gerar assinaturas para mensagens suspeitas
@@ -20,12 +19,9 @@ function mensagens_sem_similaridade = minHash(mensagens, caminho_json_treino)
             % Gerar shingles
             shingles = gerar_shingles(texto_mensagem);
 
-            % Criar vetor binário para shingles
-            vocabulario = unique(shingles);
-            vetor_binario_shingles = ismember(vocabulario, shingles);
-
             % Gerar assinatura MinHash
-            assinaturas_suspeitas(idx, :) = calcular_minHash(vetor_binario_shingles, k);
+            assinaturas_suspeitas(idx, :) = calcular_minHash(shingles, k);
+            disp(['MinHash Signature for message ', num2str(idx), ': ', num2str(assinaturas_suspeitas(idx, :))]);
         end
     end
 
@@ -40,12 +36,9 @@ function mensagens_sem_similaridade = minHash(mensagens, caminho_json_treino)
             % Gerar shingles
             shingles = gerar_shingles(texto_mensagem);
 
-            % Criar vetor binário para shingles
-            vocabulario = unique(shingles);
-            vetor_binario_shingles = ismember(vocabulario, shingles);
-
             % Gerar assinatura MinHash
-            assinaturas_treino(idx, :) = calcular_minHash(vetor_binario_shingles, k);
+            assinaturas_treino(idx, :) = calcular_minHash(shingles, k);
+            disp(['MinHash Signature for training message ', num2str(idx), ': ', num2str(assinaturas_treino(idx, :))]);
         end
     end
 
@@ -58,23 +51,14 @@ function mensagens_sem_similaridade = minHash(mensagens, caminho_json_treino)
         for j = 1:num_treino
             similaridade = calcular_similaridade(assinaturas_suspeitas(i, :), assinaturas_treino(j, :));
             similaridades(i, j) = similaridade;
+            if similaridade > limiar_similaridade
+                mensagens_com_similaridade{end+1} = mensagens{i};
+            end
             max_similaridade = max(max_similaridade, similaridade); % Atualizar o valor máximo
-            mensagens_com_similaridade{end+1} = mensagens{i};
         end
         % Exibir a mensagem e o valor máximo de similaridade encontrado
         disp(['Mensagem: ', mensagens{i}]);
         disp(['Maior similaridade: ', num2str(max_similaridade)]);
-    end
-
-    % Converter mensagens para strings manualmente (sem usar cellfun)
-    mensagens_str = cell(1, num_mensagens);
-    for i = 1:num_mensagens
-        mensagens_str{i} = char(mensagens{i});
-    end
-
-    mensagens_com_similaridade_str = cell(1, length(mensagens_com_similaridade));
-    for i = 1:length(mensagens_com_similaridade)
-        mensagens_com_similaridade_str{i} = char(mensagens_com_similaridade{i});
     end
 
     % Converter mensagens para strings manualmente (sem usar cellfun)
@@ -98,40 +82,45 @@ function mensagens_sem_similaridade = minHash(mensagens, caminho_json_treino)
             mensagens_sem_similaridade{end+1} = mensagens{i};
         end
     end
-
 end
 
+% Função para gerar shingles (n-grams)
 function shingles = gerar_shingles(texto)
-    shingles = lower(split(texto));
-    shingles = shingles(~cellfun('isempty', shingles));
-    disp(shingles);
+    % Tokenizar texto e criar shingles
+    tokens = lower(strsplit(texto));
+    shingles = {};
+    for i = 1:(length(tokens) - 1) % Gerar bigrams (shingles de 2 palavras)
+        shingles{end+1} = strjoin(tokens(i:i+1));
+    end
 end
 
 % Função para calcular assinaturas MinHash
-function assinaturas = calcular_minHash(vetor_binario, k)
-    % Número de shingles
-    n = length(vetor_binario);
-
+function assinaturas = calcular_minHash(shingles, k)
     % Inicializar assinaturas com infinito
     assinaturas = inf(1, k);
+    
+    % Funções de dispersão (hashes)
+    a = randi([1, 2^32-1], 1, k); 
+    b = randi([0, 2^32-1], 1, k); 
 
-    % Funções de dispersão
-    prime = 2147483647; 
-    a = randi([1, prime-1], 1, k); 
-    b = randi([0, prime-1], 1, k); 
-
-    % Percorrer cada posição do vetor binário
-    for i = 1:n
-        if vetor_binario(i) == 1
-            for j = 1:k
-                hash_value = mod(mod(a(j) * i + b(j), prime), n);
-                assinaturas(j) = min(assinaturas(j), hash_value);
-            end
+    % Gerar assinatura MinHash
+    for i = 1:length(shingles)
+        shingle_hash = string2hash(shingles{i}); % Hash do shingle
+        for j = 1:k
+            hash_value = mod(a(j) * shingle_hash + b(j), 2^32);
+            assinaturas(j) = min(assinaturas(j), hash_value);
         end
     end
+    disp(['Generated MinHash Signature: ', num2str(assinaturas)]);
 end
 
 % Função para calcular similaridade entre assinaturas
 function similaridade = calcular_similaridade(assinatura1, assinatura2)
     similaridade = sum(assinatura1 == assinatura2) / length(assinatura1);
+    disp(['Calculated similarity: ', num2str(similaridade)]);
+end
+
+% Função para gerar um hash a partir de uma string
+function hash = string2hash(str)
+    hash = mod(sum(double(char(str))), 2^32);
 end
